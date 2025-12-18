3bf0ad21aae32263046a551353cc8e4abe5e0631
Prathik Shetty <prathikshetty006@gmail.com>
2025-12-18 19:20:12 +0530

    working now lol

--- dist/users/users.controller.d.ts
+++ dist/users/users.controller.d.ts
@@ -1,2 +1,12 @@
+import { UsersService } from './users.service';
+import { CreateUserDto } from './create-user.dto';
+import { UpdateUserDto } from './update-user.dto';
 export declare class UsersController {
+    private readonly userService;
+    constructor(userService: UsersService);
+    create(createUserDto: CreateUserDto): Promise<import("./user.entity").User>;
+    findAll(): Promise<import("./user.entity").User[]>;
+    findOne(id: number): Promise<import("./user.entity").User>;
+    update(id: number, updateUserDto: UpdateUserDto): Promise<import("./user.entity").User>;
+    remove(id: number): Promise<void>;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ParseIntPipe,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './create-user.dto';
+import { UpdateUserDto } from './update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Put('id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
+  }
+}
